8ecfe40dc6642a616c3d6066cd4852e9fb19b225
Michael Kaufman <MLKaufman@users.noreply.github.com>
2023-12-07 12:57:59 -0700

    add a new argument prefix output

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,18 @@ fn main() -> io::Result<()> {
     let start_time = Instant::now();
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} <barcode_file> <fastq_folder> [output_folder]", args[0]);
+    if args.len() < 4 {
+        eprintln!("Usage: {} <barcode_file> <fastq_folder> <output_folder> [prefix]", args[0]);
         std::process::exit(1);
     }
 
     let barcode_file = &args[1];
     let fastq_folder = &args[2];
-    let output_folder = args.get(3).map(|s| s.as_str()).unwrap_or(".");
+    let output_folder = &args[3];
+    let prefix = args.get(4).map(|s| s.as_str()).unwrap_or("");
 
     let mut barcode_map: HashMap<String, String> = HashMap::new();
+
 
     let barcode_reader = BufReader::new(File::open(barcode_file)?);
     for line in barcode_reader.lines().skip(1) {
@@ -56,7 +58,8 @@ fn main() -> io::Result<()> {
                 let read_id = header.split(' ').next().unwrap().trim_start_matches('@');
                 let barcode = barcode_map.get(read_id).map(|s| s.as_str()).unwrap_or_else(|| "unknown");
 
-                let output_path = Path::new(output_folder).join(format!("{}.fastq.gz", barcode));
+                let output_filename = format!("{}{}.fastq.gz", prefix, barcode);
+                let output_path = Path::new(output_folder).join(output_filename);
 
                 let encoder = file_encoders.entry(barcode.to_string())
                     .or_insert_with(|| {
@@ -84,7 +87,7 @@ fn main() -> io::Result<()> {
     for (barcode, count) in count_map.iter() {
         let percentage = (*count as f64) / (total_count as f64) * 100.0;
         println!("{}: {:.2}% ({} reads)", barcode, percentage, count);
-}
+    }
 
-Ok(())  
+    Ok(())  
 }
