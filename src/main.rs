62cab36f3fd51b13bc51a73e4234bf30167f1149
Michael Kaufman <MLKaufman@users.noreply.github.com>
2023-12-18 11:58:17 -0700

    dont sort unclassified

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use flate2::read::MultiGzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::{self, File, OpenOptions};
 use std::io::{self, BufRead, BufReader, Write};
@@ -24,7 +24,6 @@ fn main() -> io::Result<()> {
 
     let mut barcode_map: HashMap<String, String> = HashMap::new();
 
-
     let barcode_reader = BufReader::new(File::open(barcode_file)?);
     for line in barcode_reader.lines().skip(1) {
         let line = line?;
@@ -34,6 +33,11 @@ fn main() -> io::Result<()> {
         }
     }
 
+    println!("Number of read IDs loaded from demultiplex file: {}", barcode_map.len());
+
+    let mut processed_reads: HashSet<String> = HashSet::new();
+    let mut non_unique_read_ids: HashSet<String> = HashSet::new();
+    let mut reads_without_barcode = 0;
     let mut file_encoders: HashMap<String, GzEncoder<File>> = HashMap::new();
     let mut count_map: HashMap<String, usize> = HashMap::new();
     let mut total_count = 0;
@@ -49,15 +53,29 @@ fn main() -> io::Result<()> {
             let reader = BufReader::new(decoder);
 
             for chunk in reader.lines().collect::<Result<Vec<_>, _>>()?.chunks_exact(4) {
-                total_count += 1;
+                if chunk.len() != 4 {
+                    eprintln!("Incomplete read found: {:?}", chunk);
+                    continue;
+                }
+
                 let header = &chunk[0];
+                let read_id = header.split(' ').next().unwrap().trim_start_matches('@');
+
+                if !processed_reads.insert(read_id.to_string()) {
+                    non_unique_read_ids.insert(read_id.to_string());
+                }
+
+                if !barcode_map.contains_key(read_id) {
+                    reads_without_barcode += 1;
+                    continue;
+                }
+
+                total_count += 1;
                 let seq = &chunk[1];
                 let plus = &chunk[2];
                 let qual = &chunk[3];
 
-                let read_id = header.split(' ').next().unwrap().trim_start_matches('@');
                 let barcode = barcode_map.get(read_id).map(|s| s.as_str()).unwrap_or_else(|| "unknown");
-
                 let output_filename = format!("{}{}.fastq.gz", prefix, barcode);
                 let output_path = Path::new(output_folder).join(output_filename);
 
@@ -83,11 +101,13 @@ fn main() -> io::Result<()> {
 
     println!("Sorting complete. Time taken: {:?}", duration);
     println!("Total number of reads: {}", total_count);
+    println!("Number of non-unique read IDs in FASTQ files: {}", non_unique_read_ids.len());
+    println!("Number of reads without a corresponding barcode: {}", reads_without_barcode);
     println!("Summary:");
     for (barcode, count) in count_map.iter() {
         let percentage = (*count as f64) / (total_count as f64) * 100.0;
         println!("{}: {:.2}% ({} reads)", barcode, percentage, count);
     }
 
-    Ok(())  
+    Ok(())
 }
